449b594acaa12eed63fb36458ea52d5924cd0cc6
notdanna <afrancor1900@alumno.ipn.mx>
2024-12-19 22:27:24 -0600

    Spring Initializr

---
+++ src/main/java/com/hoc/compiler/hoCompiler/HoCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.hoc.compiler.hoCompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoCompilerApplication.class, args);
+	}
+
+}
